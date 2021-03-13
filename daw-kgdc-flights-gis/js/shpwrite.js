e30b9986799dccc3c97b7350e24566f55c423977
daw-kgdc <77790007+daw-kgdc@users.noreply.github.com>
2021-03-13 21:25:54 +0530

    Add files via upload

--- daw-kgdc-flights-gis/js/shpwrite.js
+++ daw-kgdc-flights-gis/js/shpwrite.js
@@ -13121,42 +13121,27 @@ module.exports.line = justType('LineString', 'POLYLINE');
 module.exports.polygon = justType('Polygon', 'POLYGON');
 
 function justType(type, TYPE) {
-    return function(gj) {
-        var oftype = gj.features.filter(isType(type));
-        var geometries;
-        if (TYPE === 'POLYGON') {
-            geometries = [oftype.map(justCoords)];
-        } else if (TYPE === 'POLYLINE') {
-            geometries = oftype.map(function(t) { return [justCoords(t)]; });
-        } else {
-            geometries = oftype.map(justCoords);
-        }
-        return {
-            geometries: geometries,
-            properties: oftype.map(justProps),
-            type: TYPE
-        };
+  return function(gj) {
+    var oftype = gj.features.filter(isType(type));
+    return {
+      geometries: oftype.map(justCoords),
+      properties: oftype.map(justProps),
+      type: TYPE
     };
+  };
 }
 
 function justCoords(t) {
-    if (t.geometry.coordinates[0] !== undefined &&
-        t.geometry.coordinates[0][0] !== undefined &&
-        t.geometry.coordinates[0][0][0] !== undefined) {
-        return t.geometry.coordinates[0];
-    } else {
-        return t.geometry.coordinates;
-    }
+  return t.geometry.coordinates;
 }
 
 function justProps(t) {
-    return t.properties;
+  return t.properties;
 }
 
 function isType(t) {
-    return function(f) { return f.geometry.type === t; };
+  return function(f) { return f.geometry.type.replace('Multi', '') === t; };
 }
-
 },{}],59:[function(require,module,exports){
 var ext = require('./extent');
 
