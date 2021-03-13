a3d359f5aa79b2b5a0095ec99844e627aa14089d
daw-kgdc <77790007+daw-kgdc@users.noreply.github.com>
2021-03-13 21:39:59 +0530

    Add files via upload

--- daw-kgdc-flights-gis/js/shpwrite.js
+++ daw-kgdc-flights-gis/js/shpwrite.js
@@ -13122,9 +13122,17 @@ module.exports.polygon = justType('Polygon', 'POLYGON');
 
 function justType(type, TYPE) {
   return function(gj) {
-    var oftype = gj.features.filter(isType(type));
+    var oftype;
+    var geometries;
+    if (TYPE === 'POLYLINE') {
+        oftype = gj.features.filter(isTypeLineString(type));
+        geometries = oftype.map(function(t) { return [justCoords(t)]; });
+    } else {
+        oftype = gj.features.filter(isTypeOthers(type));
+        geometries = oftype.map(justCoords);
+    }
     return {
-      geometries: oftype.map(justCoords),
+      geometries: geometries,
       properties: oftype.map(justProps),
       type: TYPE
     };
@@ -13139,9 +13147,13 @@ function justProps(t) {
   return t.properties;
 }
 
-function isType(t) {
-  return function(f) { return f.geometry.type.replace('Multi', '') === t; };
+function isTypeLineString(t) {
+    return function(f) { return f.geometry.type === t; };
 }
+
+function isTypeOthers(t) {
+    return function(f) { return f.geometry.type.replace('Multi', '') === t; };
+  }
 },{}],59:[function(require,module,exports){
 var ext = require('./extent');
 
