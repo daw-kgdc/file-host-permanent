e4d2d3bece7058d366d10b1467c05f7f734e3b8e
daw-kgdc <77790007+daw-kgdc@users.noreply.github.com>
2021-03-22 23:24:57 +0530

    Add files via upload

--- daw-kgdc-flights-gis/js/shpwrite.js
+++ daw-kgdc-flights-gis/js/shpwrite.js
@@ -17064,7 +17064,7 @@ var zip = require('./zip');
 var saveAs = require('file-saver').saveAs;
 
 module.exports = function(gj, options) {
-  zip(gj, options).then(function(blob) { saveAs(blob, 'kmls-shapefiles.zip'); });
+  zip(gj, options).then(function(blob) { saveAs(blob, options.file + '.zip'); });
 };
 
 },{"./zip":114,"file-saver":32}],107:[function(require,module,exports){
